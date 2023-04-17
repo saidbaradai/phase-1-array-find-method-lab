fa594c8d868a4d938c2e96e3db40ab4736f893b3
said baradai <saitbaradai@gmail.com>
2023-04-17 07:26:51 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+
+// i did it like this at first 
+
+// function superbowlWin(array){
+//     for (const o of array) {
+//         if(o.result=== "W")
+//         return o.year
+//     }
+// }
+
+
+function superbowlWin(array){
+
+    const result=array.find(e=>e.result==="W")
+
+    return result? result.year : undefined
+}
